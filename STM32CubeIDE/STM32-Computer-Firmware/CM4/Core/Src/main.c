c65dbfdb18ee8815a216c507b9dedba9b96189fd
danielmartensson <daniel.martensson100@outlook.com>
2024-08-02 14:05:30 +0200

    Updatec clocks for TF-A

--- STM32CubeIDE/STM32-Computer-Firmware/CM4/Core/Src/main.c
+++ STM32CubeIDE/STM32-Computer-Firmware/CM4/Core/Src/main.c
@@ -130,8 +130,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   RCC_OscInitStruct.PLL2.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL2.PLLSource = RCC_PLL12SOURCE_HSE;
-  RCC_OscInitStruct.PLL2.PLLM = 2;
-  RCC_OscInitStruct.PLL2.PLLN = 34;
+  RCC_OscInitStruct.PLL2.PLLM = 3;
+  RCC_OscInitStruct.PLL2.PLLN = 66;
   RCC_OscInitStruct.PLL2.PLLP = 2;
   RCC_OscInitStruct.PLL2.PLLQ = 2;
   RCC_OscInitStruct.PLL2.PLLR = 1;
@@ -139,22 +139,22 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL2.PLLMODE = RCC_PLL_INTEGER;
   RCC_OscInitStruct.PLL3.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL3.PLLSource = RCC_PLL3SOURCE_HSE;
-  RCC_OscInitStruct.PLL3.PLLM = 2;
-  RCC_OscInitStruct.PLL3.PLLN = 50;
-  RCC_OscInitStruct.PLL3.PLLP = 4;
-  RCC_OscInitStruct.PLL3.PLLQ = 2;
-  RCC_OscInitStruct.PLL3.PLLR = 2;
-  RCC_OscInitStruct.PLL3.PLLRGE = RCC_PLL3IFRANGE_1;
+  RCC_OscInitStruct.PLL3.PLLM = 6;
+  RCC_OscInitStruct.PLL3.PLLN = 104;
+  RCC_OscInitStruct.PLL3.PLLP = 2;
+  RCC_OscInitStruct.PLL3.PLLQ = 17;
+  RCC_OscInitStruct.PLL3.PLLR = 37;
+  RCC_OscInitStruct.PLL3.PLLRGE = RCC_PLL3IFRANGE_0;
   RCC_OscInitStruct.PLL3.PLLFRACV = 0;
   RCC_OscInitStruct.PLL3.PLLMODE = RCC_PLL_INTEGER;
   RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL4.PLLSource = RCC_PLL4SOURCE_HSE;
-  RCC_OscInitStruct.PLL4.PLLM = 2;
-  RCC_OscInitStruct.PLL4.PLLN = 40;
-  RCC_OscInitStruct.PLL4.PLLP = 4;
-  RCC_OscInitStruct.PLL4.PLLQ = 6;
-  RCC_OscInitStruct.PLL4.PLLR = 25;
-  RCC_OscInitStruct.PLL4.PLLRGE = RCC_PLL4IFRANGE_1;
+  RCC_OscInitStruct.PLL4.PLLM = 4;
+  RCC_OscInitStruct.PLL4.PLLN = 99;
+  RCC_OscInitStruct.PLL4.PLLP = 6;
+  RCC_OscInitStruct.PLL4.PLLQ = 8;
+  RCC_OscInitStruct.PLL4.PLLR = 8;
+  RCC_OscInitStruct.PLL4.PLLRGE = RCC_PLL4IFRANGE_0;
   RCC_OscInitStruct.PLL4.PLLFRACV = 0;
   RCC_OscInitStruct.PLL4.PLLMODE = RCC_PLL_INTEGER;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -168,12 +168,12 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
                               |RCC_CLOCKTYPE_PCLK3|RCC_CLOCKTYPE_PCLK4
                               |RCC_CLOCKTYPE_PCLK5;
-  RCC_ClkInitStruct.AXISSInit.AXI_Clock = RCC_AXISSOURCE_HSE;
+  RCC_ClkInitStruct.AXISSInit.AXI_Clock = RCC_AXISSOURCE_PLL2;
   RCC_ClkInitStruct.AXISSInit.AXI_Div = RCC_AXI_DIV1;
   RCC_ClkInitStruct.MCUInit.MCU_Clock = RCC_MCUSSOURCE_PLL3;
-  RCC_ClkInitStruct.MCUInit.MCU_Div = RCC_MCU_DIV1;
-  RCC_ClkInitStruct.APB4_Div = RCC_APB4_DIV1;
-  RCC_ClkInitStruct.APB5_Div = RCC_APB5_DIV1;
+  RCC_ClkInitStruct.MCUInit.MCU_Div = RCC_MCU_DIV2;
+  RCC_ClkInitStruct.APB4_Div = RCC_APB4_DIV2;
+  RCC_ClkInitStruct.APB5_Div = RCC_APB5_DIV4;
   RCC_ClkInitStruct.APB1_Div = RCC_APB1_DIV2;
   RCC_ClkInitStruct.APB2_Div = RCC_APB2_DIV2;
   RCC_ClkInitStruct.APB3_Div = RCC_APB3_DIV2;
