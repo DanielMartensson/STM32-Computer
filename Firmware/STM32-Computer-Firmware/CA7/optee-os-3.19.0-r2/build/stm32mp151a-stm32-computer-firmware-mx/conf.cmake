1889ac17c49b4cda381f45c88d9e27b604cd5ade
danielmartensson <daniel.martensson100@outlook.com>
2024-08-09 20:37:19 +0200

    UART pin changes

--- Firmware/STM32-Computer-Firmware/CA7/optee-os-3.19.0-r2/build/stm32mp151a-stm32-computer-firmware-mx/conf.cmake
+++ Firmware/STM32-Computer-Firmware/CA7/optee-os-3.19.0-r2/build/stm32mp151a-stm32-computer-firmware-mx/conf.cmake
@@ -1,5 +1,5 @@
 # auto-generated TEE configuration file
-# TEE version 7e836759b-dev
+# TEE version 9a0f8e83b-dev
 set(CFG_AES_GCM_TABLE_BASED y)
 # CFG_APDU_PTA is not set 
 set(CFG_ARM32_core y)
--- Firmware/STM32-Computer-Firmware/CA7/optee-os-3.19.0-r2/build/stm32mp151a-stm32-computer-firmware-mx/core/arch/arm/dts/.stm32mp151a-stm32-computer-firmware-mx.pre.dts.d
+++ Firmware/STM32-Computer-Firmware/CA7/optee-os-3.19.0-r2/build/stm32mp151a-stm32-computer-firmware-mx/core/arch/arm/dts/.stm32mp151a-stm32-computer-firmware-mx.pre.dts.d
@@ -13,5 +13,4 @@
  core/arch/arm/dts/stm32mp15xa.dtsi \
  core/arch/arm/dts/stm32mp15xxac-pinctrl.dtsi \
  core/include/dt-bindings/mfd/st,stpmic1.h \
- core/include/dt-bindings/power/stm32mp1-power.h \
- core/arch/arm/dts/stm32mp15-pinctrl.dtsi
+ core/include/dt-bindings/power/stm32mp1-power.h
