4c025fb20bea11fbf1cae519f0947dd84e08855b
danielmartensson <daniel.martensson100@outlook.com>
2024-08-12 12:57:35 +0200

    U-boot UART

--- Firmware/STM32-Computer-Firmware/CA7/u-boot-2022.10.r2/build/stm32mp15_defconfig/include/generated/timestamp_autogenerated.h
+++ Firmware/STM32-Computer-Firmware/CA7/u-boot-2022.10.r2/build/stm32mp15_defconfig/include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Aug 11 2024"
-#define U_BOOT_TIME "21:07:42"
+#define U_BOOT_DATE "Aug 12 2024"
+#define U_BOOT_TIME "12:46:22"
 #define U_BOOT_TZ "+0200"
-#define U_BOOT_EPOCH 1723403262
+#define U_BOOT_EPOCH 1723459582
